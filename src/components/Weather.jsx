5acf1b3a1f25bc7ba065b02160d307dcd04d3d2d
rishwanthk <iamrishwanthk@gmail.com>
2024-10-04 22:49:06 +0530

    userinput feilds

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './Weather.css';
 import search_icon from '../assets/search.png';
 import clear_icon from '../assets/clear.png';
@@ -11,7 +11,7 @@ import humidity_icon from '../assets/humidity.png';
 
 const Weather = () => {
 
-    
+    const inputRef = useRef()
     const [weatherData, setWeatherData] = useState(false);
 
     const allIcons = {
@@ -32,6 +32,11 @@ const Weather = () => {
     }
 
     const search = async (city) =>{
+        if(city === "")
+        {
+            alert("ENter city name");
+            return;
+        }
         try{
                 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import .meta.env.VITE_APP_ID}`;
 
@@ -49,7 +54,8 @@ const Weather = () => {
         }
         catch(error)
         {
-
+            setWeatherData(false);
+            console.error("Error in fetching weather data");
         }
         
     }
@@ -61,9 +67,12 @@ const Weather = () => {
     return (
         <div className="weather">
             <div className="search-bar">
-                <input type="text" placeholder="Search"/>
-                <img src={search_icon} alt=""/>
+                <input ref={inputRef} type="text" placeholder="Search"/>
+                <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
             </div>
+            {weatherData?<>
+
+
             <img src={weatherData.icon} className="weather-icon"/>
             <p className="temperature">{weatherData.temperature}°c</p>
             <p className="location">{weatherData.location}</p>
@@ -83,6 +92,7 @@ const Weather = () => {
                     </div>
                 </div>
             </div>
+            </>:<></>}
         </div>
     )
 }
